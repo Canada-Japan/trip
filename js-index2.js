662977d90e41fc05c5c6675385b940c62bf2792b
Canada-Japan <141921634+Canada-Japan@users.noreply.github.com>
2024-08-11 15:17:48 +0000

    aa

--- js-index2.js
+++ js-index2.js
@@ -2407,17 +2407,16 @@ location.href="https://drive.google.com/drive/mobile/folders/1zVciCLQCF1Zr6pDqft
 function send() {
     texta = "message";
     gas2();
-    data = "メッセージ：" + document.getElementById("co").value;
+    data = "[メッセージ：" + document.getElementById("co").value+"]";
     var url = "https://script.google.com/macros/s/AKfycbxV6_8DSnuOmJ0CBSmxK6d0OrcbPmODzmwlvUeo0psoU54xa2kz5RK8FouTEx0YRCsR/exec";
-    var datas = [];
-    datas.push(data);
     var params = {
         "method":"post",
         "mode":"no-cors",
         "Content-Type":"application/json",
-        "body":JSON.stringify(datas)
+        "body":JSON.stringify(data)
     }
     fetch(url,params);
+    console.log(JSON.stringify(data));
     alert("送信しました");
 }
 function gas2(){
